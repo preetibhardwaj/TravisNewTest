a0b3e6a12a8f8d4db60e1ab8ee62ef0218eeaae1
preeti-bhardwaj-ibm <preeti.bhardwaj@ibm.com>
2018-08-23 13:19:02 -0400

    first

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+    -H "Authorization: token _W8RChrULtM7EXErb9d4NQ" \
+    -d "$body" \
+    https://api.travis-ci.org/repo/20736023/requests
---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * IBM Confidential
+ * OCO Source Materials
+ * (C) Copyright IBM Corp. 2018
+ */
+
+package hello;
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("HelloWorld");
+    }
+
+}
